0335349c7840f5ca3056ef254417378e251164f0
Karlis <veckaganskarlis@gmail.com>
2023-02-25 16:49:55 +0200

    Handles multiple cases, outputs in txt files

--- metaverse.py
+++ metaverse.py
@@ -1,87 +1,84 @@
-import math
+T = int(input("Number of test cases: "))
 
-T = int(input("Test case num: "))
-friendsAndDays = input("Num of friends in your group and total days: ")
+results = []
 
-inputs = friendsAndDays.split()
+for x in range(T):
+    friendsAndDays = input("Num of friends in your group and total days: ")
 
-# Total number of friends
-N = int(inputs[0])
-# Total number of days
-M = int(inputs[1])
+    inputs = friendsAndDays.split()
 
-frequency = []
-Pn = []
+    # Total number of friends
+    N = int(inputs[0])
+    # Total number of days
+    M = int(inputs[1])
 
-def loginDayFrequency():
-    for x in range(N):
-        frequency = int(input(f'P {x+1}: '))
+    frequency = []
+    Pn = []
 
-        print('Friend', x+1 , 'logs in every', frequency, 'days')
+    def loginDayFrequency():
+        for x in range(N):
+            frequency = int(input(f'P {x+1}: '))
 
-        Pn.append(frequency)
+            print('Friend', x+1 , 'logs in every', frequency, 'days')
 
-loginDayFrequency()
-print(' ')
+            Pn.append(frequency)
 
-totalDaysLoggenIn = 0
+    loginDayFrequency()
+    print(' ')
 
-def loginDaysForEachUser(Pn, M):
-    for x in range(len(Pn)):
-        totalDaysLoggedIn =  math.ceil(M / Pn[x])
+    friendLists = []
+
+    def sameDayLogin():
+        original_value = 1
+        x = original_value
+
+        for i in range(N):    
+
+            global friend
+            friend = [] 
+            for j in range(M):
+                while x < M:
+                    x += Pn[i]
+                    if x <= M:
+                        friend.append(x)
+                    break
+            if x >= M:
+                x = original_value
+
+            friendLists.append(friend)    
+                        
+    sameDayLogin()
+
+    def simontaniousDays():
+        global count
+        count = 1
+
+        for i in range(len(friendLists[0])):
+
+            current_num = friendLists[0][i]
+
+            # Flag to keep track of whether the number is found in all lists
+            found_in_all_lists = True
+
+            for j in range(1, len(friendLists)):
+
+                if current_num not in friendLists[j]:
+                    found_in_all_lists = False
+                    break
+            # Increment count if the current number is found in all lists
+            if found_in_all_lists:
+                count += 1
         
-        print('Friend', x+1 , 'logged in', totalDaysLoggedIn, 'days in total')
-        
-loginDaysForEachUser(Pn, M)
+        results.append(count)
+        print(f'Case #{x+1}: {count}')
 
-friendLists = []
+    simontaniousDays()
 
-
-def sameDayLogin():
-    original_value = 1
-    x = original_value
-
-    for i in range(N):    
-
-        global friend
-        friend = [] 
-        for j in range(M):
-            while x < M:
-                x += Pn[i]
-                if x <= M:
-                    friend.append(x)
-                break
-        if x >= M:
-            x = original_value
-
-        friendLists.append(friend)    
-                      
-sameDayLogin()
-
-print(friendLists)
-
-def simontaniousDays():
-    count = 1
-
-    for i in range(len(friendLists[0])):
-
-        current_num = friendLists[0][i]
-
-        # Flag to keep track of whether the number is found in all lists
-        found_in_all_lists = True
-
-        for j in range(1, len(friendLists)):
-
-            if current_num not in friendLists[j]:
-                found_in_all_lists = False
-                break
-         # Increment count if the current number is found in all lists
-        if found_in_all_lists:
-            count += 1
-
-    # print(f'All friend same day login happened {count} times')
-    print(f'Case #{T}: {count}')
-
-simontaniousDays()
-            
- 
+    # Here I create a text file containing the output for every test case
+    if x+1 == T:
+        # Open a file in write mode
+        with open('output.txt', 'w') as file:   
+            for r in range(len(results)):
+                # write data to the file
+                file.write(f'Case #{r+1}: {results[r]}\n')
+        file.close()    
